a156e9de0d8a104d1a6a94a077db552682ba4ac9
Bernard Maltais <bmaltais@gmail.com>
2022-10-28 22:09:33 -0400

    Adding direct compression support Adding model merge tool

--- ckpt_add.py
+++ ckpt_add.py
@@ -2,24 +2,34 @@ import os
 import argparse
 import torch
 from tqdm import tqdm
+import bz2
+import pickle
+import _pickle as cPickle
 
 parser = argparse.ArgumentParser(description="Merge two models")
 parser.add_argument("model_0", type=str, help="Path to dehydrated model")
 parser.add_argument("model_1", type=str, help="Path to destination model")
 parser.add_argument("--str", type=float, help="Strength of the rehydration (-0.05..0.05)", default=0, required=False)
-
 parser.add_argument("--output", type=str, help="Output file name, without extension", default="merged", required=False)
-
 args = parser.parse_args()
 
-model_0 = torch.load(args.model_0)
+# Load any compressed pickle file
+def decompress_pickle(file):
+    data = bz2.BZ2File(file, "rb")
+    data = cPickle.load(data)
+    return data
+
+print("Loading and decompressing dehydrated model...")
+theta_0 = decompress_pickle(args.model_0) # torch.load(args.model_0)
+# model_0 = decompress_pickle(args.model_0) # torch.load(args.model_0)
+print("Loading base model...")
 model_1 = torch.load(args.model_1)
-theta_0 = model_0["state_dict"]
 theta_1 = model_1["state_dict"]
 alpha = args.str
 
 output_file = f'{args.output}'
 
+print("Hydrating model...")
 for key in tqdm(theta_0.keys(), desc="Stage 1/2: merge common keys"):
     if "model" in key and key in theta_1:
         theta_0[key] = theta_0[key] * (1 + alpha) + theta_1[key] * (1 - alpha)
